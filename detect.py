12485e41c91db617ab50d4ca86b7a65fa35e048d
huwenmanong <86091576+huwenmanong@users.noreply.github.com>
2025-10-11 14:03:34 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -11,13 +11,6 @@ if __name__ == '__main__':
                   project='runs/detect',
                   name='exp',
                   save=True,
-                  # conf=0.2,
-                  # iou=0.7,
-                  # agnostic_nms=True,
-                   visualize=True, # visualize model features maps
-                  # line_width=2, # line width of the bounding boxes
-                  # show_conf=False, # do not show prediction confidence
-                  # show_labels=False, # do not show prediction labels
-                  # save_txt=True, # save results as .txt file
-                  # save_crop=True, # save cropped images with results，保存目标裁剪
+                  visualize=True, # visualize model features maps
+
                 )
