02431093cf0ce84167d56d3c2a343ab2c9c7193b
huwenmanong <86091576+huwenmanong@users.noreply.github.com>
2025-10-11 14:09:44 +0800

    val.py

--- val.py
+++ val.py
@@ -8,9 +8,6 @@ if __name__ == '__main__':
               split='test',
               imgsz=640,
               batch=4,
-              # iou=0.7,
-              # rect=False,
-              # save_json=True, # if you need to cal coco metrice
               project='runs/val',
               name='exp',
               )
