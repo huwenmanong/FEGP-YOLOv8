482d162379a2fc5c7406dbcee3fe143c72f793f1
huwenmanong <86091576+huwenmanong@users.noreply.github.com>
2025-10-11 14:02:30 +0800

    val.py

--- val.py
+++ val.py
@@ -1,11 +1,6 @@
 import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
-#本脚本用于验证或测试训练好的YOLO模型的性能
-# BILIBILI UP 魔傀面具
-# 验证参数官方详解链接：https://docs.ultralytics.com/modes/val/#usage-examples:~:text=of%20each%20category-,Arguments%20for%20YOLO%20Model%20Validation,-When%20validating%20YOLO
-#本脚本用于验证或测试YOLO模型的性能，根据需要，将split设置为val或test：
-# 精度小数点保留位数修改问题可看<使用说明.md>下方的<YOLOV8源码常见疑问解答小课堂>第五点
 
 if __name__ == '__main__':
     model = YOLO('runs/train/exp16/weights/best.pt')
